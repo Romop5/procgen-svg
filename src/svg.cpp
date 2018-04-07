1c093f515a8c16121d5d452129d6eb6e13f6abae
Roman Dobias <rom.dobias@gmail.com>
2018-04-07 22:37:07 +0200

    Added line

--- src/svg.cpp
+++ src/svg.cpp
@@ -1,136 +1,148 @@
-#include <procgen/procgen.h>
 #include "xml.h"
+#include <procgen/procgen.h>
 
 using namespace ProcGen;
 
-const int WIDTH  = 1024;
+const int WIDTH = 1024;
 const int HEIGHT = 1024;
 
 void saveResultAsSVGFile(json result, char* fileName);
 int main(int argc, char** argv)
 {
-	ProcGen::Procgen pg;
-	pg.setDebugOn(false);
+    ProcGen::Procgen pg;
+    pg.setDebugOn(false);
 
-	pg.parseFile("input.procgen");
-	if(pg.isReady())
-	{
-        // set uniform
+    pg.parseFile("input.procgen");
+    if (pg.isReady()) {
+	// Set uniforms
         pg.setUniform("WIDTH", WIDTH);
         pg.setUniform("HEIGHT", HEIGHT);
-        if(pg.runInit() == false)
+        if (pg.runInit() == false)
             return 1;
-		std::cout << "It's dones" << std::endl;
-		pg.run(1);
+
+	// Initialization is done, now run the derivation
+        pg.run(1);
+
+	// Serialize to JSON
         json result = pg.serialize();
-        std::cout << "Result" << result.dump(1) << "\n";
+        std::cout << "Result: " << result.dump(1) << "\n";
 
-	saveResultAsSVGFile(result, "test.svg");
-        
-	} else {
+	// Process resulting JSON tree as test.svg
+        saveResultAsSVGFile(result, "test.svg");
+
+    } else {
         std::cout << "Failed...\n";
+	return 1;
     }
+    return 0;
 }
-
 
 XML::Entity saveEllipse(json ellipse)
 {
-	XML::Entity xmlEllipse("ellipse");
-	xmlEllipse["cx"] = ellipse["x"].get<float>();
-	xmlEllipse["cy"] = ellipse["y"].get<float>();
-	xmlEllipse["rx"] = ellipse["dx"].get<float>();
-	xmlEllipse["ry"] = ellipse["dy"].get<float>();
-	return xmlEllipse;
+    XML::Entity xmlEllipse("ellipse");
+    xmlEllipse["cx"] = ellipse["x"].get<float>();
+    xmlEllipse["cy"] = ellipse["y"].get<float>();
+    xmlEllipse["rx"] = ellipse["dx"].get<float>();
+    xmlEllipse["ry"] = ellipse["dy"].get<float>();
+    return xmlEllipse;
 }
+
+XML::Entity saveLine(json line)
+{
+    XML::Entity xmlLine("line");
+    xmlLine["x1"] = line["x"].get<float>();
+    xmlLine["y1"] = line["y"].get<float>();
+    xmlLine["x2"] = line["xb"].get<float>();
+    xmlLine["y2"] = line["yb"].get<float>();
+    return xmlLine;
+}
+
 
 
 XML::Entity saveCircle(json circle)
 {
-	XML::Entity xmlCircle("circle");
-	xmlCircle["cx"] = std::to_string(circle["x"].get<float>());
-	xmlCircle["cy"] = std::to_string(circle["y"].get<float>());
-	xmlCircle["r"] = std::to_string(circle["radius"].get<float>());
-	return xmlCircle;
+    XML::Entity xmlCircle("circle");
+    xmlCircle["cx"] = std::to_string(circle["x"].get<float>());
+    xmlCircle["cy"] = std::to_string(circle["y"].get<float>());
+    xmlCircle["r"] = std::to_string(circle["radius"].get<float>());
+    return xmlCircle;
 }
 
 std::string rgbToString(json rgb)
 {
-	std::stringstream ss;
-	ss << "rgb(" << rgb["r"] << "," << rgb["g"] << "," << rgb["b"] << ")";
-	return ss.str();
+    std::stringstream ss;
+    ss << "rgb(" << rgb["r"] << "," << rgb["g"] << "," << rgb["b"] << ")";
+    return ss.str();
 }
 std::string decodeStyle(json styleCollection)
 {
 
-	if(!styleCollection.is_array())
-	{
-		return "";	
-	}
-	std::stringstream ss;
-	// for each style element in collection
-	for(auto &styleElement: styleCollection)
-	{
-		if(!styleElement.is_structured())
-			continue;
-		if(styleElement.find("_type") == styleElement.end())
-			continue;
-		// decode fillColorStyle
-		if(styleElement["_type"] == "fillColorStyle")
-		{
-			ss << "fill: " << rgbToString(styleElement["color"]) << ";";
-		}
-		// decode strokeColorStyle
-		if(styleElement["_type"] == "strokeColorStyle")
-		{
-			ss << "color: " << rgbToString(styleElement["color"]) << ";";
-		}
-
-	}
-	return ss.str();
+    if (!styleCollection.is_array()) {
+        return "";
+    }
+    std::stringstream ss;
+    // for each style element in collection
+    for (auto& styleElement : styleCollection) {
+        if (!styleElement.is_structured())
+            continue;
+        if (styleElement.find("_type") == styleElement.end())
+            continue;
+        // decode fillColorStyle
+        if (styleElement["_type"] == "fillColorStyle") {
+            ss << "fill: " << rgbToString(styleElement["color"]) << ";";
+        }
+        // decode strokeColorStyle
+        if (styleElement["_type"] == "strokeColorStyle") {
+            ss << "color: " << rgbToString(styleElement["color"]) << ";";
+        }
+    }
+    return ss.str();
 }
 
 void saveResultAsSVGFile(json result, char* fileName)
 {
-	// Create XML tree for SVG file 
-	XML::Exporter svgFile;
-	auto& svgTree = svgFile.getRoot();
-	svgTree.setName("svg");
-	svgTree["width"] = std::to_string(WIDTH);
-	svgTree["height"] = std::to_string(HEIGHT);
+    // Create XML tree for SVG file
+    XML::Exporter svgFile;
+    auto& svgTree = svgFile.getRoot();
+    svgTree.setName("svg");
+    svgTree["width"] = std::to_string(WIDTH);
+    svgTree["height"] = std::to_string(HEIGHT);
 
-	// for all top-level structures in result
-	for(auto &structure: result)	
-	{
-		// if structure doesn't have any type, than we can not process it 
-		// and thus it's discarded 
-		if(structure.find("_type") == structure.end())
-			continue;
-		// get user-defined type of structure from script
-		std::string typeOfStructure = structure["_type"].get<std::string>();	
+    // for all top-level structures in result
+    for (auto& structure : result) {
+        // if structure doesn't have any type, than we can not process it
+        // and thus it's discarded
+        if (structure.find("_type") == structure.end())
+            continue;
+        // get user-defined type of structure from script
+        std::string typeOfStructure = structure["_type"].get<std::string>();
 
-		XML::Entity resultEntity;
+        XML::Entity resultEntity;
 
-		if(typeOfStructure == "ellipse")
-			resultEntity = saveEllipse(structure);
 
-		if(typeOfStructure == "circle")
-			resultEntity = saveCircle(structure);
+        // DETECT type of primitive and parse it HERE
+        if (typeOfStructure == "ellipse")
+            resultEntity = saveEllipse(structure);
 
-		if(structure.find("style") != structure.end())
-		{
-			auto styleString = decodeStyle(structure["style"]);
-			resultEntity["style"] = styleString;
-		}	
+        if (typeOfStructure == "circle")
+            resultEntity = saveCircle(structure);
 
-		svgTree.addChild(resultEntity);
-	}
+        if (typeOfStructure == "line")
+            resultEntity = saveLine(structure);
 
-	// Ready to dump it out
-	std::cout << "Ready to dump: " << svgTree.serialize() << std::endl;
-	FILE* output = fopen(fileName, "w");
-	if(output != nullptr)
-	{
-		svgTree.dump(output);
-		fclose(output);
-	}
+        if (structure.find("style") != structure.end()) {
+            auto styleString = decodeStyle(structure["style"]);
+            resultEntity["style"] = styleString;
+        }
+
+        svgTree.addChild(resultEntity);
+    }
+
+    // Ready to dump it out
+    std::cout << "Ready to dump: " << svgTree.serialize() << std::endl;
+    FILE* output = fopen(fileName, "w");
+    if (output != nullptr) {
+        svgTree.dump(output);
+        fclose(output);
+    }
 }
--- src/xml.h
+++ src/xml.h
@@ -4,77 +4,80 @@
  * 2018, MIT License
  */
 #include <map>
-#include <vector>
 #include <sstream>
-namespace XML
-{
-	class Entity: public std::map<std::string, std::string>
-	{
-		std::vector<Entity> children;
-		// Entity name
-		std::string name;
-		public:
-			Entity(): name("entity") {}
-			Entity(std::string newname): name(newname) {}
-			~Entity() = default;
+#include <vector>
+namespace XML {
+class Entity : public std::map<std::string, std::string> {
+    std::vector<Entity> children;
+    // Entity name
+    std::string name;
 
-			std::string serialize() 
-			{
-				std::stringstream ss;
+public:
+    Entity()
+        : name("entity")
+    {
+    }
+    Entity(std::string newname)
+        : name(newname)
+    {
+    }
+    ~Entity() = default;
 
-				ss << "<" << this->name;
-				if(this->size() == 0)
-					ss << ">\n";
-				else {
-					ss << " ";
-					/*for(auto attribute: this->getAttributes())
+    std::string serialize()
+    {
+        std::stringstream ss;
+
+        ss << "<" << this->name;
+        if (this->size() == 0)
+            ss << ">\n";
+        else {
+            ss << " ";
+            /*for(auto attribute: this->getAttributes())
 					{
 						ss << attribute.first << "= '" << attribute.second << "' ";
 					}
 					*/
-					for (auto it = this->begin(); it != this->end(); ++it) {
-						ss << it->first << " = '" << it->second << "' ";
-					  }
-					ss << ">\n";
-				}
-				for(auto &child: this->children)
-				{
-					ss << child.serialize();
-				}
-				ss << "</"<< this->name << ">\n";
+            for (auto it = this->begin(); it != this->end(); ++it) {
+                ss << it->first << " = '" << it->second << "' ";
+            }
+            ss << ">\n";
+        }
+        for (auto& child : this->children) {
+            ss << child.serialize();
+        }
+        ss << "</" << this->name << ">\n";
 
-				return ss.str();
-			}
-			void dump(FILE* file)
-			{
-				fprintf(file,"%s",this->serialize().c_str()); 
-			}
+        return ss.str();
+    }
+    void dump(FILE* file)
+    {
+        fprintf(file, "%s", this->serialize().c_str());
+    }
 
-			std::map<std::string, std::string>* getAttributes() {
-				return this;
-			}
+    std::map<std::string, std::string>* getAttributes()
+    {
+        return this;
+    }
 
-			void setName(std::string name) { this->name = name; }
+    void setName(std::string name) { this->name = name; }
 
-			void addChild(Entity entity) { this->children.push_back(entity); }
-	};
+    void addChild(Entity entity) { this->children.push_back(entity); }
+};
 
-	class EntityText: public Entity
-	{
-		std::string content;
+class EntityText : public Entity {
+    std::string content;
+};
+class Exporter {
+    Entity rootEntity;
 
-	};
-	class Exporter
-	{
-		Entity rootEntity;
-		public:
-			Exporter() = default;
-			~Exporter() = default;
-			
-			Entity& getRoot() {return this->rootEntity;}
-			void dump(FILE* file = stdout)
-			{
-				this->rootEntity.dump(file);
-			}
-	};
+public:
+    Exporter() = default;
+    ~Exporter() = default;
+
+    Entity& getRoot() { return this->rootEntity; }
+    void dump(FILE* file = stdout)
+    {
+        this->rootEntity.dump(file);
+    }
+};
 };
